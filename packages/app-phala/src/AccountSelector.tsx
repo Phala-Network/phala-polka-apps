90565e18b05eeb7926507ea25db47e836dbeaf4e
h4x3rotab <h4x3rotab@gmail.com>
2019-12-16 06:05:12 +0800

    Prettify AccountSelector

--- packages/app-phala/src/AccountSelector.tsx
+++ packages/app-phala/src/AccountSelector.tsx
@@ -4,6 +4,7 @@
 
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { Grid } from 'semantic-ui-react';
 import { Bubble, InputAddress } from '@polkadot/react-components';
 import { AccountIndex, Balance, Nonce } from '@polkadot/react-query';
 
@@ -18,24 +19,32 @@ function AccountSelector ({ className, onChange }: Props): React.ReactElement<Pr
   useEffect((): void => onChange(accountId), [accountId]);
 
   return (
-    <section className={`template--AccountSelector ui--row ${className}`}>
-      <InputAddress
-        className='medium'
-        label='my default account'
-        onChange={setAccountId}
-        type='account'
-      />
-      <div className='medium'>
-        <Bubble color='teal' icon='address card' label='index'>
-          <AccountIndex value={accountId} />
-        </Bubble>
-        <Bubble color='yellow' icon='adjust' label='balance'>
-          <Balance params={accountId} />
-        </Bubble>
-        <Bubble color='yellow' icon='target' label='transactions'>
-          <Nonce params={accountId} />
-        </Bubble>
-      </div>
+    <section className={`template--AccountSelector ${className}`}>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column>
+            <InputAddress
+              className='medium'
+              label='选择身份'
+              onChange={setAccountId}
+              type='account'
+            />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row columns={1}>
+          <Grid.Column textAlign='right'>
+            <Bubble color='teal' icon='address card' label='索引'>
+              <AccountIndex value={accountId} />
+            </Bubble>
+            <Bubble color='yellow' icon='adjust' label='余额'>
+              <Balance params={accountId} />
+            </Bubble>
+            <Bubble color='yellow' icon='target' label='交易数量'>
+              <Nonce params={accountId} />
+            </Bubble>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </section>
   );
 }
